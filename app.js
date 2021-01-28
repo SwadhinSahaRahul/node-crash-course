e723a474d06b235b71abf80976894f150a043f72
Swadhin Saha <rahul4kuet2k10@gmail.com>
2021-01-28 17:24:38 +0600

    view engine ejs

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const express = require('express');
 
+// express app
 const app = express();
+
+// register view eninge
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     // res.send(`<p>Home Page</p>`);
-    res.sendFile('./views/index.html', {root: __dirname});
+    //res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: 'Blog1', snippet: "Blog Snippet1"},
+        {title: 'Blog2', snippet: "Blog Snippet2"},
+        {title: 'Blog3', snippet: "Blog Snippet3"},
+    ];
+
+    res.render('index', {
+        title: 'Home', blogs
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {
+        title: 'About'
+    });
 });
 
-app.get('/about-me', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {
+        title: 'Create Blog'
+    });
 });
+
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {
+        title: 'Not Found'
+    });
 });
 
 app.listen(3000);
