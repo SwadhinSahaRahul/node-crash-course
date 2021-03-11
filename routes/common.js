274cf3812ad1f573802983665f728d7db37e814d
Swadhin Saha <rahul4kuet2k10@gmail.com>
2021-03-11 18:28:19 +0600

    Data from database

--- routes/common.js
+++ routes/common.js
@@ -8,35 +8,39 @@ const router = express.Router();
 router.get('/', (req, res) => {
     // res.send(`<p>Home Page</p>`);
     //res.sendFile('./views/index.html', {root: __dirname});
-    const blogs = [
+    /*const blogs = [
         {title: 'Blog1', snippet: "Blog Snippet1"},
         {title: 'Blog2', snippet: "Blog Snippet2"},
         {title: 'Blog3', snippet: "Blog Snippet3"},
-    ];
+    ];*/
 
-    res.render('index', {
-        title: 'Home', blogs
-    });
+
+    Blog.find()
+        .then((blogs) => {
+            res.render('index', {
+                title: 'Home', blogs
+            });
+        })
+        .catch((error) => {
+
+        });
+
 });
 
+router.get('/blog/:id', (req, res) => {
+    Blog.findById(req.params.id)
+        .then((blog) => {
+            res.send(blog)
+        })
+        .catch((error) => {
+
+        });
+});
 
 router.get('/about', hostDetails, (req, res) => {
     res.render('about', {
         title: 'About'
     });
-});
-
-router.get('/blogs/create', (req, res) => {
-    res.render('create', {
-        title: 'Create Blog'
-    });
-});
-
-router.post('/blogs/create', (req, res) => {
-    const blog = new Blog({
-        title: req.params.title
-    });
-    blog.save();
 });
 
 module.exports = {
